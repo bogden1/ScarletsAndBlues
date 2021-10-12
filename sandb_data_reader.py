6f92b64f79f9c697513d67f9046a21ef9aeb9d03
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-10-12 12:05:29 +0100

    Add a date filter
    Allows reading of records only after a given date.

--- sandb_data_reader.py
+++ sandb_data_reader.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+from datetime import datetime
 from utils import add_to_dict_num, add_to_dict_list
 from record_reader_classes import classificationRow
 
@@ -14,11 +15,8 @@ class sandbDataReader:
         self.workflow_subject_index = {}
 
     def load_data(self, data_file_name, version = None, after_date = None):
-        # TODO: Add a date filter to only process latest transcriptions
-        if after_date is None:
-            date_limit = '1/1/2021'
-        else:
-            date_limit = after_date
+        if not after_date is None:
+            date_limit = datetime.strptime(after_date, '%Y-%m-%d') #Will fault on bad format
 
         self.data_file_name = data_file_name
         file_handle = open(data_file_name, 'r')
@@ -34,6 +32,12 @@ class sandbDataReader:
                 else: #Should be some indexable type defining min and max version nos, inclusive
                     if row_version < version[0] or row_version > version[1]:
                         continue
+            if not after_date is None:
+                str_date = json.loads(row[10])['finished_at'] #Field index 10 is the metadata
+                assert str_date[10] == 'T'
+                date = datetime.strptime(str_date[:10], '%Y-%m-%d') #Will fault on bad format
+                if date <= date_limit:
+                    continue
             R = classificationRow()
             R.add_row(row)
             self.data_rows.append(R)
